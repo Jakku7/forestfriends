80cb58b8301d4487299b50ce74102ee50190d229
Jakub <jtomgluba@gmail.com>
2020-11-29 13:49:13 +0100

    implement fix for banner

--- blog/src/templates/banner.js
+++ blog/src/templates/banner.js
@@ -1,17 +1,12 @@
 import React from 'react'  
 import Link from 'gatsby-link'
-import LottieWrapper from './lottie.js';
-
 import bannerimg from '../img/forestfriends.png'
-import LottieControl from './lottie.js';
 
 import './banner.css'
 
 const Banner = ({ data }) => (  
   <div className={"bannercontainer"}>
-    {/*<LottieWrapper />*/}
     <img src={bannerimg} />
-    <LottieControl />
     <Link to="/stepper" title="Click Here"><button className={"standard"}>Click here</button></Link>
   </div>
 )
